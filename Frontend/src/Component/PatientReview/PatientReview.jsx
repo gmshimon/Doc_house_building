ef958b083dd9613db9ca3a8404fce7123dbaa268
gmshimon <simon.rosedale99@gmail.com>
2025-03-06 23:27:17 +0100

    patient review done

--- Frontend/src/Component/PatientReview/PatientReview.jsx
+++ Frontend/src/Component/PatientReview/PatientReview.jsx
@@ -1,11 +1,84 @@
-import React from 'react';
+import React from 'react'
+import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa6'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+
+// Import required module
+import { Navigation } from 'swiper/modules'
 const PatientReview = () => {
-    return (
-        <section>
-            <h1 className='text-4xl font-semibold'>Our Expert Doctors</h1>
-        </section>
-    );
-};
+  const data = [1, 2, 3]
+  return (
+    <section className='mt-10'>
+      <h1 className='text-4xl font-semibold text-center'>Our Expert Doctors</h1>
+      <p className='text-center mt-2 my-5'>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo.
+      </p>
 
-export default PatientReview;
+      <Swiper navigation={true} modules={[Navigation]} className='mySwiper'>
+        {data.map((item, index) => (
+          <SwiperSlide key={index} className='w-full'>
+            <div className='md:flex md:space-x-5 md:space-y-0 mx-6 space-y-5'>
+              <div className='border rounded-lg py-8 px-6'>
+                <div className='flex items-center justify-between'>
+                  <div className='flex items-center'>
+                    <img
+                      alt=''
+                      src='https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                      className='inline-block size-16 rounded-full ring-2 ring-[#F7A582]'
+                    />
+                    <div className='ml-3'>
+                      <p className='font-semibold text-lg'>Awlad Hossain</p>
+                      <p>Product Designer</p>
+                    </div>
+                  </div>
+                  <p className='text-6xl text-[#F7A582]'>
+                    <FaQuoteLeft />
+                  </p>
+                </div>
+                <p className='mt-5 text-justify'>
+                  Lorem Ipsum has been the industry’s standard dummy text ever
+                  since the 1500s, when an unknow printer tool a galley of type
+                  and scrambled it to make type specimen book has survived not
+                  only five centurines.
+                </p>
+              </div>
+              <div className='border rounded-lg py-8 px-6'>
+                <div className='flex items-center justify-between'>
+                  <div className='flex items-center'>
+                    <img
+                      alt=''
+                      src='https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                      className='inline-block size-16 rounded-full ring-2 ring-[#F7A582]'
+                    />
+                    <div className='ml-3'>
+                      <p className='font-semibold text-lg'>Awlad Hossain</p>
+                      <p>Product Designer</p>
+                    </div>
+                  </div>
+                  <p className='text-6xl text-[#F7A582]'>
+                    <FaQuoteLeft />
+                  </p>
+                </div>
+                <p className='mt-5 text-justify'>
+                  Lorem Ipsum has been the industry’s standard dummy text ever
+                  since the 1500s, when an unknow printer tool a galley of type
+                  and scrambled it to make type specimen book has survived not
+                  only five centurines.
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    
+    </section>
+  )
+}
+
+export default PatientReview
